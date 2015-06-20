0f6db1bd094de816dc1ab143281961372731296d
rwangnet <rwangnet@gmail.com>
2015-06-20 19:54:18 -0300

    Commit Inicial Proyecto Ejemplo Capa APlicación

---
+++ capa-aplicacion-ejemplo/src/test/java/cl/cnr/aplicacion/capa_aplicacion_ejemplo/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package cl.cnr.aplicacion.capa_aplicacion_ejemplo;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest
+{
+   @Test
+   public void testGetText() {
+      HelloWorld fixture = new HelloWorld();
+      assert "Hello World!".equals(fixture.getText());
+   }
+}
